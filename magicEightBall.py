bc11ecd4e51af8dd67560c2e7332d08031ac9724
Dave Taylor <davehtaylor@me.com>
2014-11-19 16:33:58 -0500

    Added license info

--- magicEightBall.py
+++ magicEightBall.py
@@ -1,7 +1,9 @@
 # Magic Eight Ball
 #
-# This is a Magic Eight Ball program, written in Python. A user is
-# given a prompt to ask a question of the Magic Eight Ball,
+# This is a Magic Eight Ball program, written in Python and licensed
+# under the GPLv3. See LICENSE.txt for more information.
+#
+# A user is given a prompt to ask a question of the Magic Eight Ball,
 # and then to press a key to shake the ball and receive an answer.
 # Just as with its real-life counterpart, there are 20 possible
 # answers, some negative, some positive, some neutral. This program

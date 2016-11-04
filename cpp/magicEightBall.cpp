5b5850ae292cd4aba8cfacdf69178fb52b0a38d6
Dave Taylor <davehtaylor@me.com>
2016-11-03 22:14:17 -0400

    Updated the random number generator for a more modern approach.

--- cpp/magicEightBall.cpp
+++ cpp/magicEightBall.cpp
@@ -1,13 +1,18 @@
+// A Magic Eight Ball program that gives the user a random answer from a list
+// of responses from an actual Magic Eight Ball
+
+
 #include <iostream>
 #include <string>
-#include <chrono> //seconds(), time()
-#include <thread> //sleep_for()
-#include <random> //srand(), rand()
+#include <chrono> //std::chrono::seconds()
+#include <thread> //std::this_thread::sleep_for()
+#include <random> //std::default_random_engine()
+                  //std::uniform_int_distribution
 
+
+//Give some pause to appear to be thinking about the answer
 void thinking()
 {
-    //Give some pause to appear to be thinking about the answer
-
     int i = 1;
 
     std::cout << "Contemplating ";
@@ -25,36 +30,37 @@ void thinking()
 
 int main()
 {
-    // Variables to print the title banner
     std::string stars(26, '*');
     std::string spaces(5, ' ');
 
-    // Random index to choose from the responses array
-    std::srand(std::time(NULL));
-    int rand_index = std::rand() % 19;
+    unsigned int seed = 
+        std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine rand_generator;
+    rand_generator.seed(seed);
+    std::uniform_int_distribution<int> response_range(0,19);
+    int rand_index = response_range(rand_generator);
 
-    // Eight ball responses
-    std::string responses[] = {"It is certain",
-                                "It is decidedly so",
-                                "Without a doubt",
-                                "Yes definitely",
-                                "You may rely on it",
-                                "As I see it, yes",
-                                "Most likely",
-                                "Outlook good",
-                                "Yes",
-                                "Signs point to yes",
-                                "Reply hazy try again",
-                                "Ask again later",
-                                "Better not tell you now",
-                                "Cannot predict now",
-                                "Concentrate and ask again",
-                                "Don't count on it",
-                                "My reply is no",
-                                "My sources say no",
-                                "Outlook not so good",
-                                "Very doubtful"};
-    
+    std::string responses[] {"It is certain",
+                             "It is decidedly so",
+                             "Without a doubt",
+                             "Yes definitely",
+                             "You may rely on it",
+                             "As I see it, yes",
+                             "Most likely",
+                             "Outlook good",
+                             "Yes",
+                             "Signs point to yes",
+                             "Reply hazy try again",
+                             "Ask again later",
+                             "Better not tell you now",
+                             "Cannot predict now",
+                             "Concentrate and ask again",
+                             "Don't count on it",
+                             "My reply is no",
+                             "My sources say no",
+                             "Outlook not so good",
+                             "Very doubtful"};
+  
     // Print a pretty banner
     std::cout << std::endl;
     std::cout << stars << std::endl;
@@ -72,11 +78,9 @@ int main()
     // Wait for the user to press enter
     std::getchar();
 
-    // Print the thinking animation
     thinking();
     std::cout << std::endl;
 
-    // Print the response
     std::cout << responses[rand_index] << std::endl;
     std::cout << std::endl;
 
